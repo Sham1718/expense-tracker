ee6349317f969ddfb403bc4ca69406065aa82cf1
Sham1718 <119720894+Sham1718@users.noreply.github.com>
2026-02-16 11:43:59 +0530

    Backend Completed

--- backend/controller/transactionController.js
+++ backend/controller/transactionController.js
@@ -52,3 +52,153 @@ export const deleteTransaction = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const getSummary = async (req, res) => {
+  try {
+    const summary = await Transaction.aggregate([
+      {
+        $group: {
+          _id: "$type",
+          totalAmount: { $sum: "$amount" }
+        }
+      }
+    ]);
+
+    let totalIncome = 0;
+    let totalExpense = 0;
+
+    summary.forEach(item => {
+      if (item._id === "income") {
+        totalIncome = item.totalAmount;
+      } else if (item._id === "expense") {
+        totalExpense = item.totalAmount;
+      }
+    });
+
+    res.status(200).json({
+      totalIncome,
+      totalExpense,
+      netBalance: totalIncome - totalExpense
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+export const getMonthlyAnalytics = async (req, res) => {
+  try {
+    const analytics = await Transaction.aggregate([
+      {
+        $group: {
+          _id: {
+            year: { $year: "$date" },
+            month: { $month: "$date" },
+            type: "$type"
+          },
+          totalAmount: { $sum: "$amount" }
+        }
+      },
+      {
+        $group: {
+          _id: {
+            year: "$_id.year",
+            month: "$_id.month"
+          },
+          totals: {
+            $push: {
+              type: "$_id.type",
+              amount: "$totalAmount"
+            }
+          }
+        }
+      },
+      {
+        $project: {
+          _id: 0,
+          year: "$_id.year",
+          month: "$_id.month",
+          totalIncome: {
+            $sum: {
+              $map: {
+                input: "$totals",
+                as: "item",
+                in: {
+                  $cond: [
+                    { $eq: ["$$item.type", "income"] },
+                    "$$item.amount",
+                    0
+                  ]
+                }
+              }
+            }
+          },
+          totalExpense: {
+            $sum: {
+              $map: {
+                input: "$totals",
+                as: "item",
+                in: {
+                  $cond: [
+                    { $eq: ["$$item.type", "expense"] },
+                    "$$item.amount",
+                    0
+                  ]
+                }
+              }
+            }
+          }
+        }
+      },
+      {
+        $addFields: {
+          netBalance: { $subtract: ["$totalIncome", "$totalExpense"] }
+        }
+      },
+      {
+        $sort: { year: -1, month: -1 }
+      }
+    ]);
+
+    res.status(200).json(analytics);
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+export const category=async(req,res)=>{
+  try {
+    const category = await Transaction.aggregate([
+      {
+        $match:{
+          type:"expense"
+        }
+      },
+      {
+      $group:{
+        _id:"$category",
+        totalAmount:{$sum:"$amount"}
+        
+      }
+    },
+{
+      $project:{
+        _id:0,
+        category:"$_id",
+        totalAmount:1
+      }
+      },
+      {
+        $sort:{totalAmount:-1}
+      }
+    ])
+    res.status(200).json(category);
+    
+  } catch (error) {
+    res.status(500).json({message:error.message});
+  }
+
+}
--- backend/routes/transactionRoute.js
+++ backend/routes/transactionRoute.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { createTransaction,getTransactions,deleteTransaction } from "../controller/transactionController.js";
+import { createTransaction,getTransactions,deleteTransaction,getSummary, getMonthlyAnalytics, category } from "../controller/transactionController.js";
 
 const router = express.Router();
 
 router.post("/", createTransaction);
 router.get("/",getTransactions);
 router.delete("/:id",deleteTransaction)
+router.get("/summary",getSummary);
+router.get("/month",getMonthlyAnalytics);
+router.get("/category",category);
 
 export default router;
