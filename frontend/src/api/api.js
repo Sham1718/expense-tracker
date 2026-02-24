ecb6e8a23abe52847bad4f4af336c95cbe99414f
Shyam <119720894+Sham1718@users.noreply.github.com>
2026-02-24 14:47:44 +0530

    Update API base URL to include '/api' path

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:"https://expense-tracker-1-bl8s.onrender.com"
+    baseURL:"https://expense-tracker-1-bl8s.onrender.com/api"
 })
 
 api.interceptors.request.use((req)=>{
